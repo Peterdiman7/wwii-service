1964c5a47fc0188858e13de799af53a16e72b0ff
Peterdiman7 <peter.dim.an7@gmail.com>
2025-08-12 17:48:01 +0300

    Add Battle many-to-many relation

--- src/main/java/com/peter/wwii_service/model/Battle.java
+++ src/main/java/com/peter/wwii_service/model/Battle.java
@@ -3,8 +3,12 @@ package com.peter.wwii_service.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -22,8 +26,23 @@ public class Battle {
 
     private String imgUrl;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "country_id", referencedColumnName = "id")
+    // Many-to-Many relationship with Country
+    @ManyToMany(mappedBy = "battles", fetch = FetchType.LAZY)
     @JsonBackReference
-    private Country country;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Country> countries = new ArrayList<>();
+
+    // Helper methods for managing the relationship
+    public void addCountry(Country country) {
+        if (!countries.contains(country)) {
+            countries.add(country);
+            country.getBattles().add(this);
+        }
+    }
+
+    public void removeCountry(Country country) {
+        countries.remove(country);
+        country.getBattles().remove(this);
+    }
 }
